56d6d30e829ce61b6e9385a6b3b59c786327f507
戒酒的李白 <670939375@qq.com>
2024-07-23 21:14:59 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,session,request,redirect,render_template
+import re
+app = Flask(__name__)
+app.secret_key = 'The author of this system is BaiFu.'
+
+app.register_blueprint(page.pb)
+app.register_blueprint(user.ub)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return session.clear()
+
+@app.before_request
+def before_reuqest():
+    pat = re.compile(r'^/static')
+    if re.search(pat,request.path):return
+    elif request.path == '/user/login' or request.path == '/user/register':return
+    elif session.get('username'):return
+    return redirect('/user/login')
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run()
